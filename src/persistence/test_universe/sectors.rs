ee26fe661acea9f7af2974367a2838f2e94ed2cd
jacudibu <jacudibu@gmail.com>
2024-07-25 08:07:02 +0200

    Added simple save data for sector features

--- src/persistence/test_universe/sectors.rs
+++ src/persistence/test_universe/sectors.rs
@@ -1,5 +1,7 @@
 use crate::persistence::test_universe::coordinates::{BOTTOM_LEFT, CENTER, RIGHT, TOP_RIGHT};
-use crate::persistence::{SaveDataCollection, SectorAsteroidSaveData, SectorSaveData};
+use crate::persistence::{
+    SaveDataCollection, SectorAsteroidSaveData, SectorFeatureSaveData, SectorSaveData,
+};
 use bevy::math::Vec2;
 
 pub fn create_test_data() -> SaveDataCollection<SectorSaveData> {
@@ -9,7 +11,7 @@ pub fn create_test_data() -> SaveDataCollection<SectorSaveData> {
 
     let mut sectors = SaveDataCollection::<SectorSaveData>::default();
     sectors.add(CENTER);
-    sectors.add(RIGHT);
+    sectors.add(RIGHT).with_feature(SectorFeatureSaveData::Star);
     sectors.add(TOP_RIGHT).with_asteroid_data(asteroid_data);
     sectors.add(BOTTOM_LEFT);
 
