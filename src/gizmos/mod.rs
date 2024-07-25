4d05d696369240b3354a47cdc060a521190e7698
jacudibu <jacudibu@gmail.com>
2024-07-25 06:18:02 +0200

    Added orbit circles for selected planets

--- src/gizmos/mod.rs
+++ src/gizmos/mod.rs
@@ -1,4 +1,5 @@
 mod gate_connection;
+mod orbits;
 mod plugin;
 mod sector_outlines;
 mod selected_ship_tasks;
--- src/gizmos/plugin.rs
+++ src/gizmos/plugin.rs
@@ -1,6 +1,4 @@
-use crate::gizmos::gate_connection::{draw_gate_connections, GateConnectionGizmos};
-use crate::gizmos::sector_outlines::{draw_sector_outlines, SectorOutlineGizmos};
-use crate::gizmos::selected_ship_tasks;
+use crate::gizmos::{gate_connection, orbits, sector_outlines, selected_ship_tasks};
 use crate::map_layout::MapLayout;
 use bevy::app::Update;
 use bevy::prelude::{App, AppGizmoBuilder, Plugin, Startup};
@@ -9,15 +7,17 @@ pub struct GizmoPlugin;
 impl Plugin for GizmoPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<MapLayout>()
-            .init_gizmo_group::<SectorOutlineGizmos>()
-            .init_gizmo_group::<GateConnectionGizmos>()
+            .init_gizmo_group::<sector_outlines::SectorOutlineGizmos>()
+            .init_gizmo_group::<gate_connection::GateConnectionGizmos>()
             .init_gizmo_group::<selected_ship_tasks::SelectedShipTaskGizmos>()
-            .add_systems(Startup, selected_ship_tasks::configure)
+            .init_gizmo_group::<orbits::OrbitLineGizmos>()
+            .add_systems(Startup, (selected_ship_tasks::configure, orbits::configure))
             .add_systems(
                 Update,
                 (
-                    draw_sector_outlines,
-                    draw_gate_connections,
+                    sector_outlines::draw_sector_outlines,
+                    gate_connection::draw_gate_connections,
+                    orbits::draw_orbit_circles,
                     selected_ship_tasks::draw_selected_ship_task,
                 ),
             );
