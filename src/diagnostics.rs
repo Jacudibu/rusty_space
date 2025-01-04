423d846f2d79d435f032aa8cd26fa6e8d80cd76f
jacudibu <jacudibu@gmail.com>
2025-01-04 18:14:03 +0100

    Added a tick display to our diagnostics block

--- src/diagnostics.rs
+++ src/diagnostics.rs
@@ -1,5 +1,9 @@
+use crate::simulation::prelude::SimulationTime;
 use bevy::app::{App, Plugin};
-use bevy::prelude::{Commands, Startup};
+use bevy::ecs::system::lifetimeless::SRes;
+use bevy::ecs::system::SystemParam;
+use bevy::prelude::{Commands, Component, Startup};
+use iyes_perf_ui::entry::PerfUiEntry;
 use iyes_perf_ui::prelude::*;
 
 pub struct DiagnosticsPlugin;
@@ -9,6 +13,7 @@ impl Plugin for DiagnosticsPlugin {
             .add_plugins(bevy::diagnostic::EntityCountDiagnosticsPlugin)
             .add_plugins(bevy::diagnostic::SystemInformationDiagnosticsPlugin)
             .add_plugins(PerfUiPlugin)
+            .add_perf_ui_simple_entry::<PerfUiTickDisplay>()
             .add_systems(Startup, init);
     }
 }
@@ -16,6 +21,7 @@ impl Plugin for DiagnosticsPlugin {
 fn init(mut commands: Commands) {
     commands.spawn((
         PerfUiRoot::default(),
+        PerfUiTickDisplay::default(),
         PerfUiEntryFPS {
             #[cfg(debug_assertions)]
             label: "FPS [RUNNING IN DEBUG]".into(),
@@ -30,7 +36,49 @@ fn init(mut commands: Commands) {
             color_gradient: ColorGradient::new_preset_gyr(50000.0, 200000.0, 500000.0).unwrap(),
             ..Default::default()
         },
-        PerfUiEntryCpuUsage::default(),
-        PerfUiEntryMemUsage::default(),
+        PerfUiEntryCpuUsage {
+            label: "System CPU Usage".into(),
+            ..Default::default()
+        },
+        PerfUiEntryMemUsage {
+            label: "System RAM Usage".into(),
+            ..Default::default()
+        },
     ));
+}
+
+/// Custom Perf UI entry to show the current game tick
+#[derive(Component)]
+#[require(PerfUiRoot)]
+struct PerfUiTickDisplay {
+    /// Required to ensure the entry appears in the correct place in the Perf UI
+    pub sort_key: i32,
+}
+
+impl Default for PerfUiTickDisplay {
+    fn default() -> Self {
+        Self {
+            sort_key: iyes_perf_ui::utils::next_sort_key(),
+        }
+    }
+}
+
+impl PerfUiEntry for PerfUiTickDisplay {
+    type SystemParam = SRes<SimulationTime>;
+    type Value = u32;
+
+    fn label(&self) -> &str {
+        "Tick"
+    }
+
+    fn sort_key(&self) -> i32 {
+        self.sort_key
+    }
+
+    fn update_value(
+        &self,
+        param: &mut <Self::SystemParam as SystemParam>::Item<'_, '_>,
+    ) -> Option<Self::Value> {
+        Some(param.tick())
+    }
 }
