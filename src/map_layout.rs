e346fa5cc8e72717188572d922fb46bdfe4a0f5e
jacudibu <jacudibu@gmail.com>
2025-02-01 19:25:18 +0100

    Updated dependencies

--- src/map_layout.rs
+++ src/map_layout.rs
@@ -13,21 +13,19 @@ impl Default for MapLayout {
     fn default() -> Self {
         let layout = HexLayout {
             orientation: HexOrientation::Pointy,
-            hex_size: Vec2::splat(constants::SECTOR_SIZE),
-            invert_y: true,
-            ..Default::default()
+            scale: Vec2::splat(constants::SECTOR_SIZE),
+            origin: Vec2::ZERO,
         };
 
         let mut outline_layout = layout.clone();
-        outline_layout.hex_size *= constants::SECTOR_AREA_PERCENTAGE;
+        outline_layout.scale *= constants::SECTOR_AREA_PERCENTAGE;
 
         let hex_edge_vertices = outline_layout.all_edge_coordinates(Hex::ZERO);
 
         MapLayout {
             hex_layout: HexLayout {
                 orientation: HexOrientation::Pointy,
-                hex_size: Vec2::splat(constants::SECTOR_SIZE),
-                invert_y: true,
+                scale: Vec2::splat(constants::SECTOR_SIZE),
                 ..Default::default()
             },
             hex_edge_vertices,
