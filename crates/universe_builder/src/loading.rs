c8378c7e3bce1cd15ae89e8c48fbdd3647fe12ff
jacudibu <jacudibu@gmail.com>
2025-05-11 11:08:12 +0200

    Added a makeshift loading status display

--- crates/universe_builder/src/loading.rs
+++ crates/universe_builder/src/loading.rs
@@ -1,12 +1,16 @@
+mod loading_gui;
+
 use crate::builders::ship_builder::convert_behavior_save_data_to_builder_data;
 use crate::builders::station_builder;
 use crate::builders::station_builder::parse_shipyard_save_data;
 use bevy::app::{App, Plugin, Update};
 use bevy::ecs::system::SystemParam;
 use bevy::prelude::{
-    AppExtStates, Commands, IntoScheduleConfigs, NextState, Query, Res, ResMut, StateSet,
+    AppExtStates, Commands, Condition, IntoScheduleConfigs, NextState, Query, Res, ResMut,
+    Resource, State, StateSet,
 };
 use bevy::prelude::{SubStates, in_state};
+use bevy_egui::EguiContextPass;
 use common::components::celestials::Celestial;
 use common::components::ship_velocity::ShipVelocity;
 use common::components::{BuyOrders, Sector, SectorWithCelestials};
@@ -28,13 +32,11 @@ use persistence::data::{
     GatePairSaveData, SaveDataCollection, SectorSaveData, ShipSaveData, StationSaveData,
 };
 
-/// Parses save data into entities.
-pub struct UniverseLoadingPlugin;
-
 #[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, SubStates)]
 #[source(ApplicationState = ApplicationState::LoadingUniverse)]
 pub(crate) enum LoadingState {
     #[default]
+    Initialize,
     Sectors,
     Gates,
     Stations,
@@ -42,12 +44,29 @@ pub(crate) enum LoadingState {
     Done,
 }
 
+impl LoadingState {
+    /// Returns the state that should be executed after this state.
+    fn next(&self) -> LoadingState {
+        match self {
+            LoadingState::Initialize => LoadingState::Sectors,
+            LoadingState::Sectors => LoadingState::Gates,
+            LoadingState::Gates => LoadingState::Stations,
+            LoadingState::Stations => LoadingState::Ships,
+            LoadingState::Ships => LoadingState::Done,
+            LoadingState::Done => panic!("We are already done, there is no next state!"),
+        }
+    }
+}
+
+/// Parses save data into entities.
+pub struct UniverseLoadingPlugin;
 impl Plugin for UniverseLoadingPlugin {
     fn build(&self, app: &mut App) {
         app.add_sub_state::<LoadingState>();
         app.add_systems(
             Update,
             (
+                init.run_if(in_state(LoadingState::Initialize)),
                 spawn_all_sectors.run_if(in_state(LoadingState::Sectors)),
                 spawn_all_gates.run_if(in_state(LoadingState::Gates)),
                 spawn_all_stations.run_if(in_state(LoadingState::Stations)),
@@ -55,10 +74,64 @@ impl Plugin for UniverseLoadingPlugin {
                 done.run_if(in_state(LoadingState::Done)),
             ),
         );
+
+        app.add_systems(
+            EguiContextPass,
+            loading_gui::display_loading_information.run_if(
+                in_state(LoadingState::Sectors).or(in_state(LoadingState::Gates)
+                    .or(in_state(LoadingState::Stations).or(in_state(LoadingState::Ships)))),
+            ),
+        );
     }
 }
 
-fn done(mut next_state: ResMut<NextState<ApplicationState>>) {
+// Counts how many objects needs to be loaded in total in order to calculate progress.
+#[derive(Resource)]
+struct LoadingCounts {
+    sector_count: usize,
+    gate_count: usize,
+    station_count: usize,
+    ship_count: usize,
+}
+
+/// The first step during loading. This is where we initialize our resources.
+fn init(
+    mut commands: Commands,
+    state: Res<State<LoadingState>>,
+    mut next_state: ResMut<NextState<LoadingState>>,
+    sectors: Res<SaveDataCollection<SectorSaveData>>,
+    gates: Res<SaveDataCollection<GatePairSaveData>>,
+    stations: Res<SaveDataCollection<StationSaveData>>,
+    ships: Res<SaveDataCollection<ShipSaveData>>,
+) {
+    commands.insert_resource(SectorIdMap::default());
+    commands.insert_resource(AsteroidIdMap::default());
+    commands.insert_resource(CelestialIdMap::default());
+    commands.insert_resource(GateIdMap::default());
+    commands.insert_resource(StationIdMap::default());
+    commands.insert_resource(ConstructionSiteIdMap::default());
+    commands.insert_resource(ShipIdMap::default());
+
+    commands.insert_resource(LoadingCounts {
+        sector_count: sectors.data.len(),
+        gate_count: gates.data.len(),
+        station_count: stations.data.len(),
+        ship_count: ships.data.len(),
+    });
+
+    next_state.set(state.next());
+}
+
+/// The final step during loading.
+/// We are cleaning up any temporary data that's been constructed during the loading process here.
+fn done(mut commands: Commands, mut next_state: ResMut<NextState<ApplicationState>>) {
+    commands.remove_resource::<LoadingCounts>();
+
+    commands.remove_resource::<SaveDataCollection<SectorSaveData>>();
+    commands.remove_resource::<SaveDataCollection<GatePairSaveData>>();
+    commands.remove_resource::<SaveDataCollection<StationSaveData>>();
+    commands.remove_resource::<SaveDataCollection<ShipSaveData>>();
+
     next_state.set(ApplicationState::InGame);
 }
 
@@ -68,37 +141,36 @@ pub(crate) struct SpawnAllSectorArgs<'w, 's> {
     sprites: Res<'w, SpriteHandles>,
     map_layout: Res<'w, MapLayout>,
     asteroid_manifest: Res<'w, AsteroidManifest>,
+    sector_id_map: ResMut<'w, SectorIdMap>,
+    asteroid_id_map: ResMut<'w, AsteroidIdMap>,
+    planet_id_map: ResMut<'w, CelestialIdMap>,
 }
 
 pub(crate) fn spawn_all_sectors(
-    data: Res<SaveDataCollection<SectorSaveData>>,
+    mut data: ResMut<SaveDataCollection<SectorSaveData>>,
+    state: Res<State<LoadingState>>,
     mut next_state: ResMut<NextState<LoadingState>>,
     mut args: SpawnAllSectorArgs,
 ) {
-    let mut sector_id_map = SectorIdMap::default();
-    let mut asteroid_id_map = AsteroidIdMap::default();
-    let mut planet_id_map = CelestialIdMap::default();
-    for builder in &data.data {
-        let coordinate = builder.coordinate;
-        let entity = spawn_sector(
-            &mut args.commands,
-            &args.map_layout.hex_layout,
-            builder.coordinate,
-            &builder.features,
-            &args.sprites,
-            &mut asteroid_id_map,
-            &mut planet_id_map,
-            &args.asteroid_manifest,
-        );
-        sector_id_map.insert(coordinate, entity);
+    if data.data.is_empty() {
+        next_state.set(state.next());
+        return;
     }
 
-    args.commands
-        .remove_resource::<SaveDataCollection<SectorSaveData>>();
-    args.commands.insert_resource(sector_id_map);
-    args.commands.insert_resource(asteroid_id_map);
-    args.commands.insert_resource(planet_id_map);
-    next_state.set(LoadingState::Gates);
+    let next = data.data.pop().unwrap();
+
+    let coordinate = next.coordinate;
+    let entity = spawn_sector(
+        &mut args.commands,
+        &args.map_layout.hex_layout,
+        next.coordinate,
+        &next.features,
+        &args.sprites,
+        &mut args.asteroid_id_map,
+        &mut args.planet_id_map,
+        &args.asteroid_manifest,
+    );
+    args.sector_id_map.insert(coordinate, entity);
 }
 
 #[derive(SystemParam)]
@@ -108,33 +180,32 @@ pub(crate) struct SpawnAllGatesArgs<'w, 's> {
     sectors: Query<'w, 's, (&'static mut Sector, Option<&'static SectorWithCelestials>)>,
     celestials: Query<'w, 's, &'static Celestial>,
 
-    sector_id_map: Res<'w, SectorIdMap>,
+    sector_id_map: ResMut<'w, SectorIdMap>,
+    gate_id_map: ResMut<'w, GateIdMap>,
 }
 
 pub(crate) fn spawn_all_gates(
-    data: Res<SaveDataCollection<GatePairSaveData>>,
+    mut data: ResMut<SaveDataCollection<GatePairSaveData>>,
+    state: Res<State<LoadingState>>,
     mut next_state: ResMut<NextState<LoadingState>>,
     mut args: SpawnAllGatesArgs,
 ) {
-    let mut gate_id_map = GateIdMap::new();
-
-    for data in &data.data {
-        spawn_gate_pair(
-            &mut args.commands,
-            &mut gate_id_map,
-            &mut args.sectors,
-            &args.sprites,
-            data.from_id,
-            data.from_position.to_sector_position(&args.sector_id_map),
-            data.to_id,
-            data.to_position.to_sector_position(&args.sector_id_map),
-        )
+    if data.data.is_empty() {
+        next_state.set(state.next());
+        return;
     }
 
-    args.commands
-        .remove_resource::<SaveDataCollection<GatePairSaveData>>();
-    args.commands.insert_resource(gate_id_map);
-    next_state.set(LoadingState::Stations);
+    let next = data.data.pop().unwrap();
+    spawn_gate_pair(
+        &mut args.commands,
+        &mut args.gate_id_map,
+        &mut args.sectors,
+        &args.sprites,
+        next.from_id,
+        next.from_position.to_sector_position(&args.sector_id_map),
+        next.to_id,
+        next.to_position.to_sector_position(&args.sector_id_map),
+    )
 }
 
 #[derive(SystemParam)]
@@ -146,71 +217,56 @@ pub(crate) struct SpawnAllStationsArgs<'w, 's> {
     items: Res<'w, ItemManifest>,
     recipes: Res<'w, RecipeManifest>,
     celestials: Query<'w, 's, &'static Celestial>,
+
+    station_id_map: ResMut<'w, StationIdMap>,
+    construction_site_id_map: ResMut<'w, ConstructionSiteIdMap>,
 }
 
 pub(crate) fn spawn_all_stations(
-    data: Res<SaveDataCollection<StationSaveData>>,
+    mut data: ResMut<SaveDataCollection<StationSaveData>>,
+    state: Res<State<LoadingState>>,
     mut next_state: ResMut<NextState<LoadingState>>,
     mut args: SpawnAllStationsArgs,
 ) {
-    let mut station_id_map = StationIdMap::new();
-    let mut construction_site_id_map = ConstructionSiteIdMap::new();
-    for data in &data.data {
-        build(
-            data,
-            &mut args,
-            &mut station_id_map,
-            &mut construction_site_id_map,
-        );
+    if data.data.is_empty() {
+        next_state.set(state.next());
+        return;
     }
 
-    args.commands
-        .remove_resource::<SaveDataCollection<StationSaveData>>();
-    args.commands.insert_resource(station_id_map);
-    args.commands.insert_resource(construction_site_id_map);
-
-    next_state.set(LoadingState::Ships);
-}
-
-fn build(
-    data: &StationSaveData,
-    args: &mut SpawnAllStationsArgs,
-    station_id_map: &mut StationIdMap,
-    construction_site_id_map: &mut ConstructionSiteIdMap,
-) {
+    let next = data.data.pop().unwrap();
     let sector_entity = args
         .sector_id_map
-        .get_entity(&data.position.sector)
+        .get_entity(&next.position.sector)
         .unwrap();
 
     // TODO: All custom trade data is lost right now
-    let buys = data
+    let buys = next
         .buy_orders
         .clone()
         .map_or_else(Vec::new, |x| x.orders.iter().map(|x| x.item_id).collect());
-    let sells = data
+    let sells = next
         .sell_orders
         .clone()
         .map_or_else(Vec::new, |x| x.orders.iter().map(|x| x.item_id).collect());
 
-    let production = data
+    let production = next
         .production_modules
         .clone() // TODO: Can we get rid of those clones?
         .map(station_builder::parse_production_save_data);
-    let shipyard = data.shipyard_modules.clone().map(parse_shipyard_save_data);
+    let shipyard = next.shipyard_modules.clone().map(parse_shipyard_save_data);
 
-    let data = StationSpawnData {
-        id: data.id,
-        name: data.name.clone(),
+    let next = StationSpawnData {
+        id: next.id,
+        name: next.name.clone(),
         sector_position: SectorPosition {
             sector: *sector_entity,
-            local_position: data.position.local_position,
+            local_position: next.position.local_position,
         },
         shipyard,
         production,
         buys,
         sells,
-        construction_site: data.construction_site.clone().map(|x| {
+        construction_site: next.construction_site.clone().map(|x| {
             ConstructionSiteSpawnData::new(x.queue, BuyOrders::default()) // TODO
                 .with_progress(x.current_progress)
         }),
@@ -219,12 +275,12 @@ fn build(
     spawn_station(
         &mut args.commands,
         &mut args.sectors,
-        station_id_map,
-        construction_site_id_map,
+        &mut args.station_id_map,
+        &mut args.construction_site_id_map,
         &args.sprites,
         &args.items,
         &args.recipes,
-        data,
+        next,
     );
 }
 
@@ -234,36 +290,48 @@ pub(crate) struct SpawnAllShipsArgs<'w, 's> {
     sectors: Query<'w, 's, &'static mut Sector>,
     sector_id_map: Res<'w, SectorIdMap>,
     ship_configurations: Res<'w, ShipConfigurationManifest>,
+
+    ship_id_map: ResMut<'w, ShipIdMap>,
 }
 
 pub(crate) fn spawn_all_ships(
-    data: Res<SaveDataCollection<ShipSaveData>>,
+    mut data: ResMut<SaveDataCollection<ShipSaveData>>,
+    state: Res<State<LoadingState>>,
     mut next_state: ResMut<NextState<LoadingState>>,
     mut args: SpawnAllShipsArgs,
 ) {
-    let mut ship_id_map = ShipIdMap::new();
-    for data in &data.data {
-        spawn_ship(
-            &mut args.commands,
-            data.id,
-            data.name.clone(),
-            &mut args.sectors,
-            args.sector_id_map.id_to_entity()[&data.position.sector],
-            data.position.local_position,
-            data.rotation_degrees,
-            ShipVelocity {
-                forward: data.forward_velocity,
-                angular: data.angular_velocity,
-            },
-            convert_behavior_save_data_to_builder_data(data.behavior),
-            &mut ship_id_map,
-            args.ship_configurations.get_by_id(&data.config_id).unwrap(),
-        );
+    if data.data.is_empty() {
+        next_state.set(state.next());
+        return;
     }
 
-    args.commands
-        .remove_resource::<SaveDataCollection<ShipSaveData>>();
-    args.commands.insert_resource(ship_id_map);
+    /// Arbitrary number of ships that should be loaded during a single frame.
+    /// As long as bigger frame rates = faster loading, this can be way higher down the line.
+    const SHIPS_LOADED_AT_ONCE: usize = 432;
 
-    next_state.set(LoadingState::Done);
+    let split_at = if data.data.len() > SHIPS_LOADED_AT_ONCE {
+        data.data.len() - SHIPS_LOADED_AT_ONCE
+    } else {
+        0
+    };
+
+    let split = data.data.split_off(split_at);
+    for next in split.into_iter() {
+        spawn_ship(
+            &mut args.commands,
+            next.id,
+            next.name.clone(),
+            &mut args.sectors,
+            args.sector_id_map.id_to_entity()[&next.position.sector],
+            next.position.local_position,
+            next.rotation_degrees,
+            ShipVelocity {
+                forward: next.forward_velocity,
+                angular: next.angular_velocity,
+            },
+            convert_behavior_save_data_to_builder_data(next.behavior),
+            &mut args.ship_id_map,
+            args.ship_configurations.get_by_id(&next.config_id).unwrap(),
+        );
+    }
 }
---
+++ crates/universe_builder/src/loading/loading_gui.rs
@@ -0,0 +1,40 @@
+use crate::loading::{LoadingCounts, LoadingState};
+use bevy::prelude::{Res, State};
+use bevy_egui::egui::Align2;
+use bevy_egui::{EguiContexts, egui};
+use persistence::data::{
+    GatePairSaveData, SaveDataCollection, SectorSaveData, ShipSaveData, StationSaveData,
+};
+
+pub(crate) fn display_loading_information(
+    mut context: EguiContexts,
+    state: Res<State<LoadingState>>,
+    counts: Res<LoadingCounts>,
+
+    sectors: Res<SaveDataCollection<SectorSaveData>>,
+    gates: Res<SaveDataCollection<GatePairSaveData>>,
+    stations: Res<SaveDataCollection<StationSaveData>>,
+    ships: Res<SaveDataCollection<ShipSaveData>>,
+) {
+    let text = match state.get() {
+        LoadingState::Initialize => "Initializing".to_string(),
+        LoadingState::Sectors => get_text("Sectors", sectors.data.len(), counts.sector_count),
+        LoadingState::Gates => get_text("Gates", gates.data.len() * 2, counts.gate_count * 2),
+        LoadingState::Stations => get_text("Stations", stations.data.len(), counts.station_count),
+        LoadingState::Ships => get_text("Ships", ships.data.len(), counts.ship_count),
+        LoadingState::Done => "Done!".to_string(),
+    };
+
+    egui::Window::new("Loading Status")
+        .anchor(Align2::CENTER_CENTER, egui::Vec2::ZERO)
+        .title_bar(false)
+        .collapsible(false)
+        .resizable(false)
+        .show(context.ctx_mut(), |ui| {
+            ui.label(text);
+        });
+}
+
+fn get_text(what: &str, remaining: usize, total: usize) -> String {
+    format!("Loading {what} ({} / {total})", total - remaining,)
+}
